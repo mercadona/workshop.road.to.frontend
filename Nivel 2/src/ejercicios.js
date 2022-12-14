b4c998f8ab776625e0f5784058d7337bdd9afe55
David <cem20903@gmail.com>
2022-12-14 16:45:07 +0100

    Add extra exercicies info

--- Nivel 2/src/ejercicios.js
+++ Nivel 2/src/ejercicios.js
@@ -1,48 +1,53 @@
+// Ejercicio 1
 // Crea una función que se llame mayorQueCinco.
 // Esta funcion recibira un parametro y devolvera true si el numero recibido es mayor que cinco 
 // o false si es menor
 
-
+// Ejercicio 2
 // Crea una funcion que se llame comprobarTexto
 // Recibira un parametro que sera el texto.
 // Devolvera true si el texto tiene mas de 50 caracteres o false si tiene menos de 50 caracteres
 
 
+// Ejercicio 3
 // Crea una funcion que se llame alMenosUno que recibira 2 parametros que seran 2 numeros
 // Devuelve true si al menos uno de los es mayor que 5
 // Si ningun es mayor que 5 devuelve false
 
-
+// Ejercicio 4
 // Crea una funcion que se llame algunTextoValido que recibira 2 parametros que seran 2 textos
 // Si alguno de los textos tiene una longitud de mas de 50 la funcion devolvera un true
 // Si no, devolvera un false
 
 
+// Ejercicio 5
 // Crea una funcion seleccionArray que recibe un array y devolvemos el segundo elemento de ese array
 
 
+// Ejercicio 6
 // Crea una funcion longitudDelArray que recibe un array y devolvemos su longitud
 
 
-
+// Ejercicio 7
 // Crea una funcion ultimoDelArray que recibe un array y devolvemos el ultimo elemento de ese array
 
-
+// Ejercicio 8
 // Crea una funcion otroElementoSeleccionado que recibe 2 parametros.
 // El primero es un array y el segundo un numero
 // devolvemos la posición siguiente del array que sea ese numero (Si recibimos un 0 pues devolveremos el segundo elemento del array)
 
 
-
+// Ejercicio 9
 // Crea una funcion que se llame arrayDeNumeros que recibe un parametro que sera un numero
 // y devolveremos un array de numeros (Si recibimos un 3 el array que devolvemos es: [1, 2, 3])
 
 
-
+// Ejercicio 10
 // Crea una funcion que se llame sumaDeArrayDeNumeros que recibe un parametro que sera un array
 // y devolveremos la suma de todos esos numeros (Si recibimos un array que sea [1, 2] devolveremos un 3)
 
 
+// Ejercicio 11
 // Creamos un una función que se llame arrayDePalabras donde recibiremos 2 parametros
 // El primero sera un string (por ejemplo, 'mercadona')
 // El segundo sera un numero (por ejemplo, 3)
@@ -50,30 +55,32 @@
 // si recibimos mercadona y 3 debemos devolver ['mercadona','mercadona','mercadona']
 
 
-
+// Ejercicio 12
 // Crea una funcion que se llame miPrimerObjeto que recibira un parametro que sera un objeto
 // Devolveremos el valor de la key nombre: (Ej objeto: { nombre: 'David' })
 
-
+// Ejercicio 13
 // Crea una funcion que se llame sumaDeCosasConObjetos que recibira un parametro que sera un objeto.
 // Este objeto tendra las keys numero1 y numero2. Devolveremos la suma de esos 2 numeros
 
 
-
+// Ejercicio 14
 // Crear una funcion que se llame masObjetos que recibira 2 parametros. Ambos seran objetos.
 // Si la key nombre de ambos objetos es igual devolveremos true
 // Si no son iguales devolveremos false
 
 
+// Ejercicio 15
 // Crea una funcion que se llame objetosYTextos que recibira 1 objeto con una key nombre que sera un text.
 // Si el nombre tiene mas de 5 letras devolveremos true, sino, devolveremos false
 
 
-
+// Ejercicio 16
 // Crea una funcion arrayConObjetos que recibir un array lleno de objetos tal que asi: { numero: 1 }
 // Devuelve la suma de todos los numeros de todos los objetos que existan en el array
 
 
+// Ejercicio 17
 // Necesitamos crear una funcion que se llame gestionandoNumeros.
 // Recibiras un numero. 
 // Si este numero es par deberemos devolver 'Es par'
@@ -82,7 +89,7 @@
 // Si es cero, deberemos de devolver 'Es cero, no quiero gestionar esto'
 
 
-
+// Ejercicio 18
 // Necesitamos crear una función que se llamara elMasChiquito
 // Recibira una array de Strings. Ej: ['Perro', 'Gato', 'Marsupilami']
 // Deberemos devolver el String mas corto
@@ -90,12 +97,13 @@
 // Si el array tiene mas de 5 elementos deberemos devolver 'Demasiado largo, paso de buscar'
 
 
-
+// Ejercicio 19
 // La funcion se tiene que llamar: findLongestWord
 // Recibira un array tal que así: [ "mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"]
 // Devuelve la palabra mas larga
 
 
+// Ejercicio 20
 // La funcion se tiene que llamar: sumNumbers
 // Recibira un array tal que así: [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 // Devuelve la suma de todos los numeros
